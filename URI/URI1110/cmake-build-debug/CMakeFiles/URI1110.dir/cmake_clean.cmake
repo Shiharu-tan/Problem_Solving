c47b06108982aedc07487e9278b7d0442e284b5e
Shiharu-tan <fatmagamal_99@yahoo.com>
2020-07-11 02:10:44 +0200

    Added a problem

---
+++ URI/URI1110/cmake-build-debug/CMakeFiles/URI1110.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/URI1110.dir/main.cpp.obj"
+  "URI1110.exe"
+  "URI1110.exe.manifest"
+  "URI1110.pdb"
+  "libURI1110.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/URI1110.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
